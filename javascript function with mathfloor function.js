9d60cd9498500dfc06fdf71c2a740c19d8c804bb
kamrullive <kamrulhasan570@gmail.com>
2020-04-26 13:38:49 +0600

    Java script funtion with challenge probleming solve using mathfloor functio.

---
+++ javascript function with mathfloor function.js
@@ -0,0 +1,25 @@
+
+function getRose(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var numberOfRose = Math.floor(money/1.5);
+  console.log("Bye "+numberOfRose+" Rose from the shop");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getRose(10);
