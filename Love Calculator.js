a30f3cb3598873c7c863c50a7bad5e6c8ef84a54
kamrullive <kamrulhasan570@gmail.com>
2020-04-29 13:07:16 +0600

    Random Part and Love calculator.

---
+++ Love Calculator.js
@@ -0,0 +1,16 @@
+//Love Calculator...
+
+var nameOne = prompt("What is Your Name? ");
+var nameTwo = prompt("What is Their Name? ");
+var loveScore = Math.floor((Math.random()*100)+1)
+alert (nameOne+ " And "+nameTwo+ " Your love Score "+loveScore+" %.");
+
+
+
+//Note Tutorial video code.
+alert("Next part code Answer");
+var n = Math.random();
+n = n*6;                    // ei khaene o hote 5 porjonto asbe.
+n = Math.floor(n)+1;        // one add korle jai porjonto dekte chai sei porjonto dekhabe
+console.log(n);
+alert("Tutorial Video Random Number "+ n);
